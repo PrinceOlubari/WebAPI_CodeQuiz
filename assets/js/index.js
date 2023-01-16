1c2c1bc4b25a2a7624c5bcf7af15093a468aeee6
Prince Olubari <olubari.prince.g@gmail.com>
2023-01-16 21:17:00 +0000

    added two functions to the index.js file

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+
+function startTimer(){
+    
+    console.log("Hello");
+}
+
+function displayQuestions(){
+    console.log("I am happy")
+}
+
+document.getElementById("start").addEventListener("click",function(){
+    startTimer();
+    displayQuestions();
+})
